4dc36bc96c878298204afc86a22bad5f12a1b806
Breno Pereira <brenopereira856@gmail.com>
2022-07-18 12:53:48 -0300

    add Vue Instance

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import { App } from './App.js';
+
+export default {
+  components: { App },
+  data() {
+    return {
+      theme: 'theme-light',
+      title: 'Quanto em reais?'
+    }
+  },
+  methods: {
+    setTheme(theme) {
+      this.theme = theme;
+      return localStorage.setItem('theme', this.theme);
+    },
+    toggleTheme() {
+      if(localStorage.getItem('theme') === 'theme-dark')
+        setTheme('theme-light')
+      else
+        setTheme('theme-dark')
+    }
+  },
+  mounted() {
+    this.setTheme(this.theme)
+  }
+}
+</script>
+
+<template :class="this.theme">
+  <app>
+    {{ title }}
+  </app>
+</template>
